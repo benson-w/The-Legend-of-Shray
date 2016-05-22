aad61438a9c9d4b5c3824395de857033301cd2db
benson-w <yuwang15@illinois.edu>
2016-05-21 22:39:12 -0500

    last commit for enemy

--- Modules/enemies/enemy1.py
+++ Modules/enemies/enemy1.py
@@ -1,5 +1,7 @@
 import pygame
 import math
+#from Modules.enemies.enemies import Enemy
+#from Modules.player.player
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -12,13 +14,11 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 class Enemy1(pygame.sprite.Sprite):
-    '''
-        not sure what to add in the parent enemy class
-    '''
 
     def __init__(self):
 
         # Call the parent's constructor
+        # Enemy.__init__(self), jk there's nothing to call, maybe once we figure enemies out
         super().__init__()
 
         self.percentage = 100
@@ -34,5 +34,21 @@ class Enemy1(pygame.sprite.Sprite):
         self.rect.x = 400
         self.rect.y = 200
 
-    #def update(self):
-        #nothing
+        # want to make actions contained so game.py isn't huger than it already is >_<
+
+        #simple movement toggle
+        self.toggle_movement = False
+
+    def update(self):
+
+        #simple movement, lmao it's not workign but honestly, FUCK it
+
+        if self.rect.x < 200:
+            self.toggle_movement == False
+        if self.rect.x > 500:
+            self.toggle_movement == True
+
+        if self.toggle_movement == False:
+            self.rect.x = self.rect.x + 1
+        if self.toggle_movement == True:
+            self.rect.x = self.rect.x - 1
